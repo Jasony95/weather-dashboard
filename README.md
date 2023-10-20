cb4f529daeb89faba8805be447029227e16e5806
Jason Yang <jasony24@outlook.com>
2023-10-19 22:03:32 -0500

    added README.md file

---
+++ README.md
@@ -0,0 +1,98 @@
+# Weather Forecast
+
+## Description
+
+- The motivation was to create a webpage to help users to see the current weather along with the next five days.
+- I build this project to help users be able to see the weather ahead.
+- It solves the problem of having to see cities weather using other tools/software.
+- I learned that there needs to be arrangement of the code in order to make it work along with structuring the data.
+
+## Table of Contents (Optional)
+
+If your README is long, add a table of contents to make it easy for users to find what they need.
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Badges](#badges)
+- [Features](#features)
+- [HowToContribute](#how-to-contribute)
+- [Test](#tests)
+
+## Installation
+
+To install the project, you will need to download the folder and then open it through any internet browser.
+
+GitHub Link: https://github.com/Jasony95/weather-dashboard
+
+GitHub Page: 
+
+## Usage
+
+Main Page without using Localstorage
+![initialpage](./assets/images/initialpage.jpg)
+After searching Saint Paul
+![firsttimesearchingpage](./assets/images/1searchresult.jpg)
+After searching Minneapolis
+![secondtimesearchingpage](./assets/images/2searchresult.jpg)
+After searching Beijing
+![searchingacityinanothercountry](./assets/images/3searchresult.jpg)
+After selecting Saint Paul Button under search
+![clickingonSaintPaulButton](./assets/images/4searchresult.jpg)
+Refreshing The Page After Using LocalStorage
+![Refreshingthepage](./assets/images/afterresultinitialpage.jpg)
+Clicking on Minneapolis Button
+![clickingonMinneapolisButton](./assets/images/afterclickoninitial.jpg)
+
+## Credits
+
+Collaborators:
+
+1. Sichoun Nplhaib Lee: https://github.com/DDXP3
+
+2. Joey Thao: https://github.com/Thaodev23
+
+Sources Used:
+
+1. https://openweathermap.org/forecast5
+2. https://coding-boot-camp.github.io/full-stack/apis/how-to-use-api-keys
+3. https://www.geeksforgeeks.org/how-to-parse-float-with-two-decimal-places-in-javascript/
+4. https://openweathermap.org/current#fields_json
+5. https://openweathermap.org/forecast5#example_JSON
+6. https://www.w3schools.com/charsets/tryit.asp?deci=8457
+7. https://www.w3schools.com/charsets/ref_utf_letterlike.asp
+8. https://stackoverflow.com/questions/44177417/how-to-display-openweathermap-weather-icon
+9. https://api.jquery.com/remove/
+10. https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_tofixed
+11. https://stackoverflow.com/questions/40985620/updating-localstorage-arrays-in-javascript
+12. https://www.geeksforgeeks.org/how-to-store-an-array-in-localstorage/
+13. https://developer.mozilla.org/en-US/docs/Web/API/Storage/getItem
+14. https://www.tutorialrepublic.com/faq/how-to-bind-click-event-to-dynamically-added-elements-in-jquery.php
+15. https://www.tutorialrepublic.com/codelab.php?topic=faq&file=jquery-bind-click-event-to-dynamically-created-elements
+16. https://www.youtube.com/watch?v=eKlBJFsiyX4&ab_channel=KnowledgeBase
+17. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes
+18. https://openweathermap.org/api/geocoding-api
+
+
+## License
+
+There is no usage for the license.
+
+## Badges
+
+No badges are used are displayed.
+
+## Features
+
+-Webpage searches for cities and forecast the current and five days.
+-Users can switch to cities that have been searched before.
+-Cities will still be displayed in the main page and can be clicked to show the current and five day weather.
+
+## How to Contribute
+
+No contribution are needed.
+
+## Tests
+
+There are no test to implement.
--- assets/js/script.js
+++ assets/js/script.js
@@ -37,9 +37,6 @@ console.log(arrayStorageLength);
 
 
 function getCurrentApi() {
-  // if (currentWeatherCard != undefined) {
-  //   currentWeatherCard.remove();
-  // }
   city = $('#city-text').val();
   // var city = "Saint Paul";
   if (city === '') {
@@ -53,24 +50,10 @@ function getCurrentApi() {
     })
     .then(function (data) {
       console.log(data);
-      // cityArr.push(city);
       console.log(cityArr);
       cityArr = arrayStorage;
       console.log(cityArr);
-      // localStorage.setItem(cities, JSON.stringify(cityArr));
-      // arrayStorage = JSON.parse(localStorage.getItem(cities));
-      // arrayStorageLength = arrayStorage.length;
       console.log("Local Length: " + arrayStorageLength);
-      // localStorage.cityArr.push('city');
-      // console.log(JSON.parse(localStorage.getItem(cities))[0]);
-      // for (var i = 0; i < JSON.parse(localStorage.getItem(cities)).length; i++) {
-      //   console.log(JSON.parse(localStorage.getItem(cities))[i]);
-      //   console.log(JSON.parse(localStorage.getItem(cities)).length);
-      // }
-      // localStorage.setItem(cities, JSON.stringify(city));
-      // console.log(localStorage.getItem(cities))
-     // .push(city);
-
       var currentDate = currentTime.format("MM/DD/YYYY");
       var currentTemp = kelvinToF(data.main.temp);
       var currentWind = data.wind.speed;
@@ -138,10 +121,8 @@ function getFutureAPI(lat, lon) {
         fiveDay = $(`<div class="container text-center">
         <h3>5-Day Forecast</h3>
         <div class="row align-items-start" id="beginDay">`);
-        // $('#dataCard').append(fiveDay);
       }
       
-      // $('#dataCard').append(fiveDay);
       for (var j = 0; j < 5; j++) {
         var nextDay = currentTime.add(dayCount, 'day').format("MM/DD/YYYY");
         dayCount++;
@@ -163,7 +144,6 @@ function getFutureAPI(lat, lon) {
       }
 
       addCityBtn(city);
-      // showCitiesBtn();
     }
   )
     
@@ -185,27 +165,11 @@ function addCityBtn(city) {
     $("#searchCard").append(newCityBtn);
     cityArr.push(city);
   }
-  // $("#searchCard").append(newCityBtn);
   localStorage.setItem(cities, JSON.stringify(cityArr));
   console.log("after adding button " + localStorage.getItem(cities));
-  // for (var i = 0; i < cityArr.length; i++) {
-  //   console.log(cityArr[i]);
-  // }
-  // for (var i = 0; i < localStorage.length; i++) {
-  //   if (cityArr.includes(localStorage.key(i))) {
-  //     return;
-  //   }
-  //   else {
-  //     console.log("city " + city);
-  //     console.log("localStorage: " + localStorage.key(i))
-  //     $("#searchCard").append(newCityBtn);
-  //     localStorage.setItem(city, city);
-  //   }
-  // }
 }
 
 function runSearch(city) {
-  // console.log($(this).$('.newCityBtn').val());
   console.log('function running.');
   for (var i = 0; i < cityArr.length; i++) {
     if (cityArr[i].includes(city)) {
@@ -231,7 +195,6 @@ $(document).ready(function() {
 function runCard(event) {
   event.preventDefault();
   city = $(this).val();
-  alert(city);
   fetchCurrentURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
   fetch(fetchCurrentURL)
     .then(function (response) {
@@ -239,24 +202,10 @@ function runCard(event) {
     })
     .then(function (data) {
       console.log(data);
-      // cityArr.push(city);
       console.log(cityArr);
       cityArr = arrayStorage;
       console.log(cityArr);
-      // localStorage.setItem(cities, JSON.stringify(cityArr));
-      // arrayStorage = JSON.parse(localStorage.getItem(cities));
-      // arrayStorageLength = arrayStorage.length;
       console.log("Local Length: " + arrayStorageLength);
-      // localStorage.cityArr.push('city');
-      // console.log(JSON.parse(localStorage.getItem(cities))[0]);
-      // for (var i = 0; i < JSON.parse(localStorage.getItem(cities)).length; i++) {
-      //   console.log(JSON.parse(localStorage.getItem(cities))[i]);
-      //   console.log(JSON.parse(localStorage.getItem(cities)).length);
-      // }
-      // localStorage.setItem(cities, JSON.stringify(city));
-      // console.log(localStorage.getItem(cities))
-      // .push(city);
-
       var currentDate = currentTime.format("MM/DD/YYYY");
       var currentTemp = kelvinToF(data.main.temp);
       var currentWind = data.wind.speed;
@@ -296,38 +245,63 @@ function runCard(event) {
       }
 
 
-      getFutureAPI(lat, lon);
+      getFutureAPI2(lat, lon);
 
     })
 }
 
-// $(document).ready(function () {
-//   cityBtn.on("click", runCard);
-// })
+function getFutureAPI2(lat, lon) {
+  fetchFutureURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey;
+  fetch(fetchFutureURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      var dayCount = 1;
+      var hourIter = 1;
 
-// cityBtn.on("click", runSearch);
-// console.log(kelvinToF(275));
+      if (fiveDay === "") {
+        fiveDay = $(`<div class="container text-center">
+        <h3>5-Day Forecast</h3>
+        <div class="row align-items-start" id="beginDay">`);
 
-// function showLocalStorage() {
-//   for (var i = 0; i < localStorage.length; i++) {
-//     if (localStorage.key(i) === localStorage.getItem(localStorage.key(i))) {
-//     var newCityBtn = $(`<button type="button" class="newCityBtn">${localStorage.key(i)}</button>`);
-//     $("#searchCard").append(newCityBtn);
-//     }
-//   }
-// }
+        $('#dataCard').append(fiveDay);
+      }
+      else {
+        fiveDay.remove();
+        fiveDay = $(`<div class="container text-center">
+        <h3>5-Day Forecast</h3>
+        <div class="row align-items-start" id="beginDay">`);
+      }
+
+      for (var j = 0; j < 5; j++) {
+        var nextDay = currentTime.add(dayCount, 'day').format("MM/DD/YYYY");
+        dayCount++;
+        var nextIcon = data.list[hourIter].weather[0].icon;
+        var nextIconURL = "http://openweathermap.org/img/w/" + nextIcon + ".png";
+        var nextTemp = kelvinToF(data.list[hourIter].main.temp);
+        var nextWind = data.list[hourIter].wind.speed;
+        var nextHum = data.list[hourIter].main.humidity;
+        nextDayCard = $(`<div class="col">
+        <h4>${nextDay}</h4>
+        <img id="icons" src="${nextIconURL}">
+          <p>Temp: ${nextTemp}&#8457;</p>
+          <p>Wind: ${nextWind} MPH</p>
+          <p>Humidity: ${nextHum}%</p>
+          </div>`);
+
+        $('#beginDay').append(nextDayCard);
+        hourIter = hourIter + 8;
+      }
+
+    
+    }
+    )
+
+}
 
 function showCitiesBtn() {
-  // for (var i = 0; i < arrayStorageLength; i++) {
-  //   var newCityBtn = $(`<button type="button" class="newCityBtn">`,JSON.parse(localStorage.getItem(cities))[${i}]`</button>`);
-  //   $("#searchCard").append(newCityBtn);
-  // }
-
-  // if (localStorage.getItem(cities) === null) {
-  //   localStorage.setItem(cities, JSON.stringify(cityArr));
-  //   arrayStorage = JSON.parse(localStorage.getItem(cities));
-  //   arrayStorageLength = JSON.parse(arrayStorage.length);
-  
 
     console.log(arrayStorageLength);
     if (arrayStorageLength != 0) {
