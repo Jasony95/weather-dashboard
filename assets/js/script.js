2318a855e0709ba8407d23a52d591fb8324a7d26
Jason Yang <jasony24@outlook.com>
2023-10-19 19:15:45 -0500

    Need to add onclick button function

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,10 +8,33 @@ var lon = 0;
 var lat = 0;
 var nextDayCard = '';
 var fiveDay = '';
-
+var cityBtn = $('.newCityBtn');
+var cityArr = [];
+var cities = 'cities';
 var fetchCurrentURL = "";
-
 var fetchFutureURL = "";
+var newCityBtn = $('.newCitybtn');
+var arrayStorage = JSON.parse(localStorage.getItem(cities));
+var arrayStorageLength = 0;
+
+console.log(arrayStorage);
+
+if (arrayStorage != null) {
+  arrayStorageLength = arrayStorage.length;
+  arrayStorage.forEach((cityName) => {
+    var newCityBtn = $(`<button type="button" class="newCityBtn">${cityName}</button>`);
+    $("#searchCard").append(newCityBtn);
+  })
+}
+
+if (arrayStorage === null) {
+  arrayStorage = [];
+}
+
+console.log(arrayStorage);
+console.log(arrayStorageLength);
+
+
 
 function getCurrentApi() {
   // if (currentWeatherCard != undefined) {
@@ -19,6 +42,10 @@ function getCurrentApi() {
   // }
   city = $('#city-text').val();
   // var city = "Saint Paul";
+  if (city === '') {
+    alert("Please type in a city.");
+    return;
+  }
   fetchCurrentURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
   fetch(fetchCurrentURL)
     .then(function (response) {
@@ -26,20 +53,34 @@ function getCurrentApi() {
     })
     .then(function (data) {
       console.log(data);
-      console.log(data.main.temp);
+      // cityArr.push(city);
+      console.log(cityArr);
+      cityArr = arrayStorage;
+      console.log(cityArr);
+      // localStorage.setItem(cities, JSON.stringify(cityArr));
+      // arrayStorage = JSON.parse(localStorage.getItem(cities));
+      // arrayStorageLength = arrayStorage.length;
+      console.log("Local Length: " + arrayStorageLength);
+      // localStorage.cityArr.push('city');
+      // console.log(JSON.parse(localStorage.getItem(cities))[0]);
+      // for (var i = 0; i < JSON.parse(localStorage.getItem(cities)).length; i++) {
+      //   console.log(JSON.parse(localStorage.getItem(cities))[i]);
+      //   console.log(JSON.parse(localStorage.getItem(cities)).length);
+      // }
+      // localStorage.setItem(cities, JSON.stringify(city));
+      // console.log(localStorage.getItem(cities))
+     // .push(city);
 
       var currentDate = currentTime.format("MM/DD/YYYY");
       var currentTemp = kelvinToF(data.main.temp);
       var currentWind = data.wind.speed;
       var currentHumidity = data.main.humidity;
       var currentIcon = data.weather[0].icon;
-      console.log(currentIcon);
       var currentIconURL = "http://openweathermap.org/img/w/" + currentIcon + ".png";
       lat = data.coord.lat.toFixed(2);
       lon = data.coord.lon.toFixed(2);
 
       if (currentWeatherCard === "") {
-        console.log("currentWeatherCard is empty string.")
         currentWeatherCard = $(`<div class="col" id="dataCard">
         <div id="topCard">
           <h2>${city} ${currentDate}<img id="icon" src=${currentIconURL} alt="Weather Icon"></h2>
@@ -54,7 +95,6 @@ function getCurrentApi() {
 
       else {
         currentWeatherCard.remove();
-        console.log("remove currentWeatherCard")
         currentWeatherCard = $(`<div class="col" id="dataCard">
         <div id="topCard">
           <h2>${city} ${currentDate}<img id="icon" src=${currentIconURL} alt="Weather Icon"></h2>
@@ -83,7 +123,6 @@ function getFutureAPI(lat, lon) {
     })
     .then(function (data) {
       console.log(data);
-      console.log(data.list[0].weather[0].icon);
       var dayCount = 1;
       var hourIter = 1;
       
@@ -96,7 +135,6 @@ function getFutureAPI(lat, lon) {
       }
       else {
         fiveDay.remove();
-        console.log("remove fiveDay");
         fiveDay = $(`<div class="container text-center">
         <h3>5-Day Forecast</h3>
         <div class="row align-items-start" id="beginDay">`);
@@ -107,18 +145,11 @@ function getFutureAPI(lat, lon) {
       for (var j = 0; j < 5; j++) {
         var nextDay = currentTime.add(dayCount, 'day').format("MM/DD/YYYY");
         dayCount++;
-        console.log("nextDay: " + nextDay);
         var nextIcon = data.list[hourIter].weather[0].icon;
         var nextIconURL = "http://openweathermap.org/img/w/" + nextIcon + ".png";
-        console.log("nextIconURL: " + nextIconURL);
         var nextTemp = kelvinToF(data.list[hourIter].main.temp);
-        console.log("nextTemp: " + nextTemp);
         var nextWind = data.list[hourIter].wind.speed;
-        console.log("nextWind: " + nextWind);
         var nextHum = data.list[hourIter].main.humidity;
-        console.log("nextHum: " + nextHum);
-        console.log("hourIter: " + hourIter);
-        console.log("dayCount: " + dayCount);
         nextDayCard = $(`<div class="col">
         <h4>${nextDay}</h4>
         <img id="icons" src="${nextIconURL}">
@@ -127,40 +158,60 @@ function getFutureAPI(lat, lon) {
           <p>Humidity: ${nextHum}%</p>
           </div>`);
 
-        $('#beginDay').append(nextDayCard); hourIter = hourIter + 8;
-        
-        // if (hourIter < 6) {
-          
-          
-        // }
-
-        // else {
-        //   hourIter++;
-        //   nextDayCard.remove();
-        //   console.log("remove nextDayCard")
-        //   nextDayCard = $(`<div class="col">
-        //   <h4>${nextDay}</h4>
-        //   <img id="icons" src="${nextIconURL}">
-        //     <p>Temp: ${nextTemp}&#8457;</p>
-        //     <p>Wind: ${nextWind} MPH</p>
-        //     <p>Humidity: ${nextHum}%</p>
-        // </div>`);
-
-
-        //   $('#beginDay').append(nextDayCard);
-        // }
-
-        // nextDayCard = $(`<div class="col">
-        //   <h4>${nextDay}</h4>
-        //   <img id="icons" src="${nextIconURL}">
-        //     <p>Temp: ${nextTemp}&#8457;</p>
-        //     <p>Wind: ${nextWind} MPH</p>
-        //     <p>Humidity: ${nextHum}%</p>
-        // </div>`);
-
-        // $('#beginDay').append(nextDayCard);
+        $('#beginDay').append(nextDayCard);
+        hourIter = hourIter + 8;
       }
-    })
+
+      addCityBtn(city);
+      // showCitiesBtn();
+    }
+  )
+    
+}
+
+function addCityBtn(city) {
+  var newCityBtn = $(`<button type="button" class="newCityBtn">${city}</button>`);
+  console.log(localStorage.getItem(cities));
+  if (localStorage.getItem(cities)===null) {
+    console.log("added " + city);
+    $("#searchCard").append(newCityBtn);
+    cityArr.push(city);
+  }
+  else if (localStorage.getItem(cities).includes(city)) {
+    console.log(city + " is duplicated.");
+    return;
+  }
+  else {
+    $("#searchCard").append(newCityBtn);
+    cityArr.push(city);
+  }
+  // $("#searchCard").append(newCityBtn);
+  localStorage.setItem(cities, JSON.stringify(cityArr));
+  console.log("after adding button " + localStorage.getItem(cities));
+  // for (var i = 0; i < cityArr.length; i++) {
+  //   console.log(cityArr[i]);
+  // }
+  // for (var i = 0; i < localStorage.length; i++) {
+  //   if (cityArr.includes(localStorage.key(i))) {
+  //     return;
+  //   }
+  //   else {
+  //     console.log("city " + city);
+  //     console.log("localStorage: " + localStorage.key(i))
+  //     $("#searchCard").append(newCityBtn);
+  //     localStorage.setItem(city, city);
+  //   }
+  // }
+}
+
+function runSearch(city) {
+  // console.log($(this).$('.newCityBtn').val());
+  console.log('function running.');
+  for (var i = 0; i < cityArr.length; i++) {
+    if (cityArr[i].includes(city)) {
+      console.log("City in localstorage");
+    }
+  }
 }
 
 function kelvinToF(kDegree) {
@@ -169,7 +220,44 @@ function kelvinToF(kDegree) {
 }
 
 apiBtn.on("click", getCurrentApi);
+// newCityBtn.on("click", runCard);
 
+// function runCard() {
+  
+// }
+
+// cityBtn.on("click", runSearch);
 // console.log(kelvinToF(275));
 
-console.log(currentTime.add(1, 'day').format("MM/DD/YYYY"));
+// function showLocalStorage() {
+//   for (var i = 0; i < localStorage.length; i++) {
+//     if (localStorage.key(i) === localStorage.getItem(localStorage.key(i))) {
+//     var newCityBtn = $(`<button type="button" class="newCityBtn">${localStorage.key(i)}</button>`);
+//     $("#searchCard").append(newCityBtn);
+//     }
+//   }
+// }
+
+function showCitiesBtn() {
+  // for (var i = 0; i < arrayStorageLength; i++) {
+  //   var newCityBtn = $(`<button type="button" class="newCityBtn">`,JSON.parse(localStorage.getItem(cities))[${i}]`</button>`);
+  //   $("#searchCard").append(newCityBtn);
+  // }
+
+  // if (localStorage.getItem(cities) === null) {
+  //   localStorage.setItem(cities, JSON.stringify(cityArr));
+  //   arrayStorage = JSON.parse(localStorage.getItem(cities));
+  //   arrayStorageLength = JSON.parse(arrayStorage.length);
+  
+
+    console.log(arrayStorageLength);
+    if (arrayStorageLength != 0) {
+      arrayStorage.forEach((cityName) => {
+        var newCityBtn = $(`<button type="button" class="newCityBtn">${cityName}</button>`);
+        $("#searchCard").append(newCityBtn);
+      })
+    }
+
+  
+}
+
